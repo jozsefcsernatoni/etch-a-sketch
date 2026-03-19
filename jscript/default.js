ca6861ad4bee199a9792900dad8c9c466e4afe3e
jozsefcsernatoni <jozsef.csernatoni@gmail.com>
2026-03-19 05:54:58 +0200

    Refactored code
    addHover and captureButton functions switched to dom style property instead of set attribute
    Added upper limit for usrSides(100)

--- jscript/default.js
+++ jscript/default.js
@@ -1,14 +1,15 @@
 const divContainer=document.querySelector(".container");
-const sqSide=50;
-const sqStyle="height: "+ sqSide + "px; border: 2px solid; width: "+ sqSide +"px; flex-basis:"+ sqSide +" px; box-sizing: border-box;"
+const widthContainer=960;
 
-function createDivs(nrSides=10){
+function createDivs(nrSides=16){
+    const sqSide=widthContainer/nrSides;
+const sqStyle="height: "+ sqSide + "px; border: 1px solid; width: "+ sqSide +"px; flex-basis:"+ sqSide +" px; box-sizing: border-box;"
 for (let i=0;i<nrSides;i++){
     for (let j=0;j<nrSides;j++){
         let sqBackground="background-color:grey;";
         let square=document.createElement("div");
         square.setAttribute("style",sqStyle+sqBackground);
-        divContainer.setAttribute("style","width:"+ nrSides* sqSide + "px;");
+        divContainer.setAttribute("style","width:"+widthContainer+"px;");
         square.classList.add("square");
         divContainer.appendChild(square);
         }
@@ -19,11 +20,26 @@ function addHover(){
     const squares=document.querySelectorAll(".square");
     for(let i=0;i<squares.length;i++){
     squares[i].addEventListener("mousemove",function(){
-        let sqBackground="background-color:green;";
-        squares[i].setAttribute("style",sqStyle+sqBackground);})
+        squares[i].style.backgroundColor = "lightblue";})
         }
     
 }
 
+function captureButton(){
+    const setSquareButton=document.querySelector("button");
+
+    setSquareButton.addEventListener("click",function(){
+        let usrSides=prompt("Please enter a positive number up to 100:", "16");
+        usrSides=usrSides<=100 ? usrSides : 100;
+        while(divContainer.firstChild){
+            divContainer.removeChild(divContainer.firstChild);
+        }
+        createDivs(usrSides);
+        addHover();
+    }
+    )
+}
+
 createDivs();
 addHover();
+captureButton();
